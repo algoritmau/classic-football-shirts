bae088ce6a0de46d8ec103fd9c884e78c214272a
Mauricio Paternina <spaceinvadev@icloud.com>
2021-10-28 17:22:09 -0500

    set up prisma with getStaticProps

--- pages/new-in.js
+++ pages/new-in.js
@@ -1,11 +1,31 @@
-export default function NewIn() {
+import { PrismaClient } from '@prisma/client'
+
+export default function NewIn({ newInShirts }) {
   return (
-    <>
+    <div>
       <h1>Club Shirts</h1>
-      <p>
-        Check out the latest additions to our amazing catalog and grab your
-        favorite item.
-      </p>
-    </>
+      <ul>
+        {newInShirts.map(newInShirt => (
+          <li key={newInShirt.id}>
+            <img src={newInShirt.image} alt={newInShirt.name} />
+            <h2>{newInShirt.name}</h2>
+            <p>{newInShirt.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const prisma = new PrismaClient()
+  const newInShirts = await prisma.shirts.findMany({
+    where: {
+      is_new_in: true
+    }
+  })
+
+  return {
+    props: { newInShirts }
+  }
 }
--- pages/shirts/clubs/index.js
+++ pages/shirts/clubs/index.js
@@ -1,8 +1,27 @@
-export default function Clubs() {
+import { PrismaClient } from '@prisma/client'
+
+export default function Clubs({ shirts }) {
   return (
-    <>
+    <div>
       <h1>Club Shirts</h1>
-      <p>Find that classic shirt of your favorite club.</p>
-    </>
+      <ul>
+        {shirts.map(shirt => (
+          <li key={shirt.id}>
+            <img src={shirt.image} alt={shirt.name} />
+            <h2>{shirt.name}</h2>
+            <p>{shirt.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const prisma = new PrismaClient()
+  const shirts = await prisma.shirts.findMany()
+
+  return {
+    props: { shirts }
+  }
 }
